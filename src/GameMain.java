e33395f263c29c75be91fcaa371f4e9533e23ab1
moneypower7 <toster1011@gmail.com>
2015-05-11 19:55:33 -0500

    Revert "Revert "Initial Work""
    This reverts commit 29fa9d8b4c22f981d8c42cbc344ca169c181e252.

---
+++ src/GameMain.java
@@ -0,0 +1,19 @@
+
+public class GameMain {
+
+	public static void main(String[] args) { //Main Idea is that the player is on a road and comes across different events as they progress, similar to board game online
+		// TODO Auto-generated method stub
+		int position,event;
+		position = 0;
+		while (position<100){
+			//event = random.nextInt(number of events);
+			//switch event
+			/*/case (any):
+			 * position = events.methodofevent(position); (The method will return the position, this way based on the event and how they handle the event the player can move forward or backward)
+			 */
+			
+		}
+
+	}
+
+}
