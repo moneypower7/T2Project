2db0e5558db7194afd410c97e9ccd44956db0c19
kesslnic000 <kesslnic000@gmail.com>
2015-05-12 21:27:45 -0500

    Misc fixes + new event

--- src/Events.java
+++ src/Events.java
@@ -1,33 +1,84 @@
 import java.util.Scanner;
-public class Events { //Everyone in the group can add their own events, they can be however funny or serious you want them to be, I have added an event so that you can see how the setup might/will look. We can adjust the rarity of each event later
+import java.util.*;
+import java.awt.*;
+
+/* important shit to do:
+ - Include checker so each event only happens once
+ - fully implement items
+ */
+
+public class Events {
 	int speed;
-	public Events(){
+	ArrayList<String> items;
+
+	public Events() {
 		speed = 2;
+		items = new ArrayList<String>();
 	}
+
 	Scanner u = new Scanner(System.in);
-	public int ev0(int p){//No event, you proceed normally @Troy
+
+	public int ev0(int p) {// No event, you proceed normally @Troy
 		int position = p;
-		System.out.println("Hmmm, that's weird... you encountered nothing on your travel, you walked forward 4 miles feeling a little bored"); //We can either use miles or spaces if we want to make it like a board game
+		System.out
+				.println("Hmmm, that's weird... you encountered nothing on your travel, you walked forward 4 miles feeling a little bored");
 		position += speed;
 		return position;
 	}
-	public int ev1(int p){ //Strange Temple (Korean Jesus) @Troy
+
+	public int ev1(int p) { // Strange Temple (Korean Jesus) @Troy
 		int position = p;
 		int choice;
-		System.out.println("You come across a strange temple, would you like to enter? (1) Yes (2) No");
+		System.out
+				.println("You come across a strange temple, would you like to enter? (1) Yes (2) No");
 		choice = u.nextInt();
-		switch (choice){
+		switch (choice) {
 		case 1:
-			System.out.println("Korean Jesus welcomes you and grants you his blessing, your speed has been increased by 2 and is now: " + speed);
+			System.out
+					.println("Korean Jesus welcomes you and grants you his blessing, your speed has been increased by 2 and is now: "
+							+ speed);
 			position += speed + 8;
+			break;
 		case 2:
-			System.out.println("Korean Jesus is disappointed in you, you walk 1 mile");
+			System.out
+					.println("Korean Jesus is disappointed in you, you walk 1 mile");
 			position += 1;
+			break;
+		default:
+			System.out
+					.println("No pity is shown to those who cannot follow the rules of the game. Die, you fiend.");
+			position = 0;
+			break;
 		}
 		return position;
 	}
-	public int ev2(int p){
+
+	public int ev2(int p) { // Deep Woods (Shulk) @Nick
 		int position = p;
+		System.out
+				.println("Deep in the woods you stumble upon an old man who asks, "
+						+ "\"Would you like to learn the way of the Monado?\"");
+		System.out.println("(1) " + "\"I'm really feeling it!\"");
+		System.out.println("(2) "
+				+ "Nah, my power levels are already over 9000.\"");
+		choice = u.nextInt();
+		switch (choice) {
+		case 1:
+			System.out
+					.println("You acquired the Monado! Use it to escape any dangerous situation.");
+			position++;
+			break;
+		case 2:
+			System.out
+					.println("You got your ass handed to you by an old man. Thank Korean Jesus the hospital was only 3 miles back.");
+			position -= 3;
+			break;
+		default:
+			System.out
+					.println("Really, dude? Just choose a valid action. Have fun in hell.");
+			position = 0;
+			break;
+		}
 		return position;
 	}
 }
